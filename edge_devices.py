3abec544e941c01c1d98e730a0828f5d61187eb9
lamducanh2005 <23020326@vnu.edu.vn>
2025-11-02 09:22:57 +0700

    create edge_devices in root

---
+++ edge_devices.py
@@ -0,0 +1,29 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+from edge_devices import EdgeDeviceContainer
+
+"""
+    edge_device.py
+    Mô phỏng thiết bị biên
+"""
+
+
+class EdgeDeviceWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Edge Devices")
+        self.setGeometry(100, 100, 1200, 1000)
+
+        self.central_widget = EdgeDeviceContainer()
+        self.setCentralWidget(self.central_widget)
+
+    def closeEvent(self, event):
+        self.central_widget.close_all()
+        event.accept()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = EdgeDeviceWindow()
+    window.show()
+    sys.exit(app.exec_())
